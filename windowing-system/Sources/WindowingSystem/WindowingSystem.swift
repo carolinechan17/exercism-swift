9996131fa89d8dae248b53d1184feb5ec1adeb82
Caroline Chan <carolinechan304@gmail.com>
2022-08-03 15:05:58 +0700

    #2: Define the position struct

--- windowing-system/Sources/WindowingSystem/WindowingSystem.swift
+++ windowing-system/Sources/WindowingSystem/WindowingSystem.swift
@@ -1,4 +1,15 @@
 // TODO: Define the Position struct
+struct Position {
+    var x: Int = 0
+    var y: Int = 0
+    
+    mutating func moveTo(newX: Int, newY: Int){
+        x = newX
+        y = newY
+    }
+}
+
+// TODO: Define the Size struct
 struct Size {
     var width: Int = 80
     var height: Int = 60
@@ -8,7 +19,5 @@ struct Size {
         height = newHeight
     }
 }
-
-// TODO: Define the Size struct
 
 // TODO: Define the Window class
