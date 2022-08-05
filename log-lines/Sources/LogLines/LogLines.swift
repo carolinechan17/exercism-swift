b39c1b94b9b7aba39eb571a7283b6625b830c358
Caroline Chan <carolinechan304@gmail.com>
2022-08-05 22:05:06 +0700

    #1: Create LogLevel enum

--- log-lines/Sources/LogLines/LogLines.swift
+++ log-lines/Sources/LogLines/LogLines.swift
@@ -1,1 +1,6 @@
 // TODO: define the 'LogLevel' enum, its `init`, and its `shortFormat` method
+
+//MARK: Create LogLevel enum
+enum LogLevel {
+    case trace, debug, info, warning, error, fatal, unknown
+}
