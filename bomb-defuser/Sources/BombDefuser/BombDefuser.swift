ccd511662719903dab789c94dc4b48eff226828a
Caroline Chan <carolinechan304@gmail.com>
2022-08-07 13:07:39 +0700

    #1: Write a closure to flip two wires

--- bomb-defuser/Sources/BombDefuser/BombDefuser.swift
+++ bomb-defuser/Sources/BombDefuser/BombDefuser.swift
@@ -1,7 +1,13 @@
 //let flip: (String, String, String) -> (String, String, String) = TODO: Please define the flip closure
-//
-//
+//MARK: Write a closure to flip two wires
+let flip = {(a: String, b: String, c: String) -> (String, String, String) in
+    let arrayOfWires: [String] = [a, b, c]
+    let sortedArrayOfWires: [String] = arrayOfWires.sorted(by: { (s1: String, s2: String) -> Bool in s1 > s2})
+    return (sortedArrayOfWires[0], sortedArrayOfWires[1], sortedArrayOfWires[2])
+}
+
 //let rotate: (String, String, String) -> (String, String, String) = TODO: Please define the stopAtOne closure
+
 
 func makeShuffle(
   flipper: @escaping ((String, String, String)) -> (String, String, String),
