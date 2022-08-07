9145469340457a271007217ddda03e4df2432aab
Caroline Chan <carolinechan304@gmail.com>
2022-08-07 15:47:42 +0700

    Solved!

--- difference-of-squares/Sources/DifferenceOfSquares/DifferenceOfSquares.swift
+++ difference-of-squares/Sources/DifferenceOfSquares/DifferenceOfSquares.swift
@@ -1,1 +1,29 @@
 //Solution goes in Sources
+import Foundation
+
+struct Squares {
+    let num: Int
+    
+    init(_ num: Int) {
+        self.num = num
+    }
+}
+
+extension Squares {
+    var squareOfSum: Int {
+        let sum : Int = (num * num + num) / 2
+        return sum * sum
+    }
+    
+    var sumOfSquares: Int {
+        var sum: Int = 0
+        for i in 0...num {
+            sum += i * i
+        }
+        return sum
+    }
+    
+    var differenceOfSquares: Int {
+        return squareOfSum - sumOfSquares
+    }
+}
