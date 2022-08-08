1060ff517f15ce0b7a4fcec94b080b21ac7af9e0
Caroline Chan <carolinechan304@gmail.com>
2022-08-08 14:18:29 +0700

    Solved!

--- gigasecond/Sources/Gigasecond/Gigasecond.swift
+++ gigasecond/Sources/Gigasecond/Gigasecond.swift
@@ -1,1 +1,21 @@
 //Solution goes in Sources
+import Foundation
+
+struct Gigasecond {
+    let gigaSecond = 1e9
+    let dateFormatter = DateFormatter()
+    var result : Date
+    
+    init?(from: String) {
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+        dateFormatter.locale = Locale(identifier: "en_US_POSSIX")
+        dateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
+        
+        let time = dateFormatter.date(from: from)
+        result = time!.addingTimeInterval(gigaSecond)
+    }
+    
+    var description : String {
+        return dateFormatter.string(from: result)
+    }
+}
