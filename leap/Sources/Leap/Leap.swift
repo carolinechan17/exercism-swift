7036ac7d378a093531becc31d3c1fe25edf870b7
Caroline Chan <carolinechan304@gmail.com>
2022-08-09 14:44:30 +0700

    Solved!

--- leap/Sources/Leap/Leap.swift
+++ leap/Sources/Leap/Leap.swift
@@ -1,1 +1,24 @@
 //Solution goes in Sources
+import Foundation
+
+struct Year {
+    let calendarYear: Int
+    
+    //MARK: Initializer for Struct Year
+    init(calendarYear: Int) {
+        self.calendarYear = calendarYear
+    }
+    
+    //MARK: Variable to decide whether the input year is leap year or not
+    var isLeapYear: Bool{
+        if calendarYear % 4 == 0 {
+            if calendarYear % 100 == 0 && calendarYear % 400 != 0 {
+                return false
+            } else {
+                return true
+            }
+        } else {
+            return false
+        }
+    }
+}
--- leap/Tests/LeapTests/LeapTests.swift
+++ leap/Tests/LeapTests/LeapTests.swift
@@ -1,4 +1,4 @@
-import XCTest
+ import XCTest
 @testable import Leap
 
 class LeapTests: XCTestCase {
