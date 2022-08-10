a78ca83edce5828050d46a39b3d72fb0a8893132
Caroline Chan <carolinechan304@gmail.com>
2022-08-10 09:45:46 +0700

    Solved!

--- rna-transcription/Sources/RnaTranscription/RnaTranscription.swift
+++ rna-transcription/Sources/RnaTranscription/RnaTranscription.swift
@@ -1,2 +1,35 @@
 // Define the 'Nucleotide' enum, its `init`, and its `complementOfDNA` method
+import Foundation
 
+//MARK: To store the error message
+enum TranscriptionError: Error {
+    case invalidNucleotide(message: String)
+}
+
+struct Nucleotide {
+    let dna: String
+    var isInvalid: String?
+    
+    init(_ dna: String) {
+        self.dna = dna
+    }
+    
+    func complementOfDNA() throws -> String {
+        var rna: String = ""
+        for char in dna {
+            switch char {
+            case "G":
+                rna += "C"
+            case "C":
+                rna += "G"
+            case "T":
+                rna += "A"
+            case "A":
+                rna += "U"
+            default:
+                throw TranscriptionError.invalidNucleotide(message: "\(char) is not a valid Nucleotide")
+            }
+        }
+        return rna
+    }
+}
