34ccdf8d6bc31d969394db74ea43b3f6ef9e82f2
Caroline Chan <carolinechan304@gmail.com>
2022-08-12 14:16:31 +0700

    Solved!

--- sum-of-multiples/Sources/SumOfMultiples/SumOfMultiples.swift
+++ sum-of-multiples/Sources/SumOfMultiples/SumOfMultiples.swift
@@ -1,1 +1,22 @@
 //Solution goes in Sources
+import Foundation
+
+struct SumOfMultiples {
+    
+    //MARK: need to add 'static' to be able to call the function like the format in the tests
+    static func toLimit(_ range: Int, inMultiples: [Int]) -> Int {
+        var sum: Int = 0
+        
+        if range > 0 {
+            for i in 1..<range {
+                for num in inMultiples {
+                    if num != 0 && i % num == 0 {
+                        sum += i
+                        break
+                    }
+                }
+            }
+        }
+        return sum
+    }
+}
