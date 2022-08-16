13521b865115c32f02e48b0904eabc063a3df026
Caroline Chan <carolinechan304@gmail.com>
2022-08-16 15:26:05 +0700

    #2 function to return amount of grains in a square

--- grains/Sources/Grains/Grains.swift
+++ grains/Sources/Grains/Grains.swift
@@ -8,5 +8,14 @@ struct Grains {
         case inputTooHigh(message: String)
     }
     
-    
+    //MARK: Function to return the amount of grains in a specific square
+    static func square(_ num: Int) throws -> UInt64 {
+        if num < 1 {
+            throw GrainsError.inputTooLow(message: "Input[\(num)] invalid. Input should be between 1 and 64 (inclusive)")
+        } else if num > 64 {
+            throw GrainsError.inputTooHigh(message: "Input[\(num)] invalid. Input should be between 1 and 64 (inclusive)")
+        } else {
+            return UInt64(pow(Double(2), Double(num-1)))
+        }
+    }
 }
