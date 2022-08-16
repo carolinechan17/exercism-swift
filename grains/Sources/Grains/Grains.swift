c5ad2b21b3229aed56d0bc7d8db11b29b111706a
Caroline Chan <carolinechan304@gmail.com>
2022-08-16 15:25:12 +0700

    #1 Enum to initialize error message

--- grains/Sources/Grains/Grains.swift
+++ grains/Sources/Grains/Grains.swift
@@ -1,1 +1,12 @@
 //Solution goes in Sources
+import Foundation
+
+struct Grains {
+    //MARK: Enum to initialized error message
+    enum GrainsError : Error {
+        case inputTooLow(message: String)
+        case inputTooHigh(message: String)
+    }
+    
+    
+}
