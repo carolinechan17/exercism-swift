bbe919d6b3063c01d86ededcf25a27e1890049e1
Caroline Chan <carolinechan304@gmail.com>
2022-08-18 15:20:36 +0700

    Solved!

--- hamming/Sources/Hamming/Hamming.swift
+++ hamming/Sources/Hamming/Hamming.swift
@@ -1,1 +1,24 @@
 //Solution goes in Sources
+import Foundation
+
+struct Hamming {
+    static func compute(_ dna: String, against: String) -> Int? {
+        let length: Int = dna.count
+        if dna.count == against.count {
+            if dna == "" && against == "" {
+                return 0
+            }
+            
+            var count: Int = 0
+            
+            for i in 0..<length {
+                if dna[dna.index(dna.startIndex, offsetBy: i)] != against[against.index(against.startIndex, offsetBy: i)] {
+                    count += 1
+                }
+            }
+            return count
+        } else {
+            return nil
+        }
+    }
+}
