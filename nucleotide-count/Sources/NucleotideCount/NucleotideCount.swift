abab49eb31b74f24dbc68c374b52e29f90f7dcc7
Caroline Chan <carolinechan304@gmail.com>
2022-08-21 12:52:11 +0700

    Solved!

--- nucleotide-count/Sources/NucleotideCount/NucleotideCount.swift
+++ nucleotide-count/Sources/NucleotideCount/NucleotideCount.swift
@@ -1,1 +1,49 @@
 //Solution goes in Sources
+import Foundation
+
+class DNA {
+    let strand: String
+    
+    init?(strand: String) {
+        var temp: String = strand.uppercased()
+        let checkCharacters: Set<Character> = ["A", "C", "G", "T"]
+        temp.removeAll(where: {checkCharacters.contains($0)})
+        
+        if temp.isEmpty {
+            self.strand = strand
+        } else {
+            return nil
+        }
+    }
+    
+    func count(_ char: Character) -> Int {
+        var count: Int = 0
+        
+        for el in strand {
+            if el == char {
+                count += 1
+            }
+        }
+        return count
+    }
+    
+    func counts() -> [String: Int] {
+        var result: [String: Int] = ["A": 0, "T": 0, "C": 0, "G": 0]
+        
+        for el in strand {
+            switch el {
+            case "A":
+                result["A"]! += 1
+            case "T":
+                result["T"]! += 1
+            case "C":
+                result["C"]! += 1
+            case "G":
+                result["G"]! += 1
+            default:
+                continue
+            }
+        }
+        return result
+    }
+}
