67f5de9fb203faf754398ca3097a1fe7d9db4e80
Caroline Chan <carolinechan304@gmail.com>
2022-08-24 09:46:51 +0700

    Solved!

--- circular-buffer/Sources/CircularBuffer/CircularBuffer.swift
+++ circular-buffer/Sources/CircularBuffer/CircularBuffer.swift
@@ -1,1 +1,44 @@
 //Solution goes in Sources
+import Foundation
+
+enum CircularBufferError: Error {
+    case bufferEmpty
+    case bufferFull
+}
+
+struct CircularBuffer<T> {
+    var buffer: [T] = []
+    let capacity: Int
+    
+    init(capacity: Int) {
+        self.capacity = capacity
+    }
+    
+    mutating func write(_ element: T) throws {
+        if buffer.count == capacity {
+            throw CircularBufferError.bufferFull
+        } else {
+            buffer.append(element)
+        }
+    }
+    
+    mutating func read() throws -> T {
+        if buffer.isEmpty {
+            throw CircularBufferError.bufferEmpty
+        } else {
+            return buffer.removeFirst()
+        }
+    }
+    
+    mutating func clear() {
+        buffer.removeAll()
+    }
+    
+    mutating func overwrite(_ element: T) {
+        if buffer.count == capacity {
+            buffer.removeFirst()
+        }
+        
+        buffer.append(element)
+    }
+}
