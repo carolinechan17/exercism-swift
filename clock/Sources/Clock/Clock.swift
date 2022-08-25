f210ef17700f957a53ec615f46a1feea7cbccfc3
Caroline Chan <carolinechan304@gmail.com>
2022-08-25 17:36:11 +0700

    Solved!

--- clock/Sources/Clock/Clock.swift
+++ clock/Sources/Clock/Clock.swift
@@ -1,1 +1,33 @@
 //Solution goes in Sources
+import Foundation
+
+//MARK: Equatable protocol to allow comparison between two objects with "==" operator
+struct Clock: Equatable, CustomStringConvertible {
+    var total: Int
+    
+    var hours: Int {total / 60}
+    var minutes: Int {total % 60}
+    
+    init(hours: Int, minutes: Int = 0) {
+        total = (minutes + hours * 60) % 1440
+        if total < 0 {
+            total += 1440
+        }
+    }
+    
+    var description: String{
+        String(format: "%02d:%02d", hours, minutes)
+    }
+    
+    func add(minutes: Int) -> Clock {
+        return Clock(hours: 0, minutes: total + minutes)
+    }
+    
+    func subtract(minutes: Int) -> Clock {
+        return Clock(hours: 0, minutes: total - minutes)
+    }
+    
+    static func ==(clock1: Clock, clock2: Clock) -> Bool {
+        return clock1.total == clock2.total
+    }
+}
