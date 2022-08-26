be936776b32dae708e34b760632086663217777b
Caroline Chan <carolinechan304@gmail.com>
2022-08-26 14:16:53 +0700

    Solved!

--- collatz-conjecture/Sources/CollatzConjecture/CollatzConjecture.swift
+++ collatz-conjecture/Sources/CollatzConjecture/CollatzConjecture.swift
@@ -1,1 +1,29 @@
 //Solution goes in Sources
+import Foundation
+
+struct CollatzConjecture {
+    enum error: Error {
+        case inputTooSmall
+    }
+    
+    static func steps(_ n: Int) throws -> Int {
+        var result: Int = 0
+        var temp: Int = n
+        
+        if temp <= 0 {
+            throw error.inputTooSmall
+        }
+        
+        while temp != 1 {
+            if temp % 2 == 0 {
+                temp /= 2
+                result += 1
+            } else {
+                temp = (3 * temp) + 1
+                result += 1
+            }
+        }
+        
+        return result
+    }
+}
