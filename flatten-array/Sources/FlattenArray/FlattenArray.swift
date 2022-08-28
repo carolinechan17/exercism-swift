744105b1143df4bbc17384101ea90d736b28317d
Caroline Chan <carolinechan304@gmail.com>
2022-08-28 14:25:49 +0700

    Solved!

--- flatten-array/Sources/FlattenArray/FlattenArray.swift
+++ flatten-array/Sources/FlattenArray/FlattenArray.swift
@@ -1,1 +1,17 @@
 //Solution goes in Sources
+import Foundation
+
+func flattenArray<T>(_ array: [Any?]) -> [T] {
+    var result: [T] = []
+    
+    for i in array {
+        if let i = i as? T {
+            result.append(i)
+        }
+        if let i = i as? [Any] {
+            result.append(contentsOf: flattenArray(i))
+        }
+    }
+    
+    return result
+}
