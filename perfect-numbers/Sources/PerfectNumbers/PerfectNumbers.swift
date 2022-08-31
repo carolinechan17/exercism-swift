7472d3892fe925ac832bd80f21f8dff73930356b
Caroline Chan <carolinechan304@gmail.com>
2022-08-31 14:01:41 +0700

    Solved!

--- perfect-numbers/Sources/PerfectNumbers/PerfectNumbers.swift
+++ perfect-numbers/Sources/PerfectNumbers/PerfectNumbers.swift
@@ -1,1 +1,32 @@
 //Solution goes in Sources
+import Foundation
+
+enum classification {
+    case perfect, abundant, deficient
+}
+
+struct NumberClassifier{
+    let number: Int
+    
+    init(number: Int) {
+        self.number = number
+    }
+    
+    var classification : classification {
+        var sum: Int = 0
+        
+        for i in 1..<number {
+            if number % i == 0 {
+                sum += i
+            }
+        }
+        
+        if sum == number {
+            return .perfect
+        } else if sum > number {
+            return .abundant
+        } else {
+            return .deficient
+        }
+    }
+}
