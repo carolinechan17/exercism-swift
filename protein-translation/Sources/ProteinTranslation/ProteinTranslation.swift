ddc10aaf0eff0a3aed08770b1d95d465eead2500
Caroline Chan <carolinechan304@gmail.com>
2022-09-02 10:36:51 +0700

    Solved!

--- protein-translation/Sources/ProteinTranslation/ProteinTranslation.swift
+++ protein-translation/Sources/ProteinTranslation/ProteinTranslation.swift
@@ -1,1 +1,48 @@
 //Solution goes in Sources
+import Foundation
+
+enum ErrorCodon: Error {
+    case invalidCodon
+}
+
+struct ProteinTranslation {
+    static func translationOfCodon(_ codon: String) throws -> String {
+        switch codon {
+        case "AUG":
+            return "Methionine"
+        case "UUU", "UUC":
+            return "Phenylalanine"
+        case "UUA", "UUG":
+            return "Leucine"
+        case "UCU", "UCC", "UCA", "UCG":
+            return "Serine"
+        case "UAU", "UAC":
+            return "Tyrosine"
+        case "UGU", "UGC":
+            return "Cysteine"
+        case "UGG":
+            return "Tryptophan"
+        case "UAA", "UAG", "UGA":
+            return "STOP"
+        default:
+            throw ErrorCodon.invalidCodon
+        }
+    }
+    
+    static func translationOfRNA(_ rna: String) throws -> [String]{
+        var ans: [String] = []
+        var temp = rna[...]
+        
+        while temp.count >= 3 {
+            let translate = try translationOfCodon(String(temp.prefix(3)))
+            switch translate {
+            case "STOP":
+                return ans
+            case let codon:
+                ans.append(codon)
+                temp.removeFirst(3)
+            }
+        }
+        return ans
+    }
+}
