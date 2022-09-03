55b73847f1374e1c6b306324c40b50f864015614
Caroline Chan <carolinechan304@gmail.com>
2022-09-03 10:39:36 +0700

    Solved!

--- proverb/Sources/Proverb/Proverb.swift
+++ proverb/Sources/Proverb/Proverb.swift
@@ -1,1 +1,18 @@
 //Solution goes in Sources
+import Foundation
+
+extension Array where Element == String {
+    func recite() -> String {
+        var result: String = ""
+        
+        for i in 0..<self.count {
+            guard i != self.count - 1 else {
+                result += "And all for the want of a \(self[0])."
+                continue
+            }
+            result += "For want of a \(self[i]) the \(self[i+1]) was lost.\n"
+        }
+        
+        return result
+    }
+}
